cba2e0e098501379ff47e83791ab02f7181ca69e
khurshedeth <khurshed.eth@gmail.com>
2024-07-04 21:54:32 +0530

    bit later but its attractive dont ignore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CardComp from "./components/CardComp";
+import FooterSection from "./components/FooterSection";
+import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
+import ReoquirementSection from "./components/ReoquirementSection";
+import SignupPage from "./components/SignupPage";
+import VerifiComp from "./components/VerifiComp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <HeroSection />
+      <SignupPage/>
+      <ReoquirementSection/>
+      <VerifiComp/>
+      <CardComp/>
+    <FooterSection/>
     </div>
   );
 }
